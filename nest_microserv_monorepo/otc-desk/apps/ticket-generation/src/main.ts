533e60638a03ba34559214a16f84222e83b3fdd8
Jasmeet-1998 <jasmeetbali.dev.2021@gmail.com>
2022-06-19 18:41:37 +0530

    dockerized ticket-generation microservices

--- nest_microserv_monorepo/otc-desk/apps/ticket-generation/src/main.ts
+++ nest_microserv_monorepo/otc-desk/apps/ticket-generation/src/main.ts
@@ -4,8 +4,10 @@ import { TicketGenerationModule } from './ticket-generation.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(TicketGenerationModule);
+  // bootstraping rabbit mq with ticket-generation
   const rmqService = app.get<RmqService>(RmqService)
   app.connectMicroservice(rmqService.getOptions('TICKETGENERATION'))
+  // use startAllMicroservices instead of app.listen
   await app.startAllMicroservices();
 }
 bootstrap();
