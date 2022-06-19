9c09b0b02086d2ddcbe87b68af1e05d82621e8c1
Jasmeet-1998 <jasmeetbali.dev.2021@gmail.com>
2022-06-19 19:15:07 +0530

    imp: rabbitmq dynamic module registeration setup

--- nest_microserv_monorepo/otc-desk/libs/common/src/rabbitmq/rmq.module.ts
+++ nest_microserv_monorepo/otc-desk/libs/common/src/rabbitmq/rmq.module.ts
@@ -1,12 +1,47 @@
 // rabbit-mq module
 
-import { Module } from "@nestjs/common";
+import { DynamicModule, Module } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { ClientsModule, Transport } from "@nestjs/microservices";
 import { RmqService } from "./rmq.service";
+
+interface RmqModuleOptions {
+    name: string;
+}
 
 @Module({
     providers: [RmqService],
     exports: [RmqService],
 })
+// 📝 making this dynamic module by making a static register method accessible by all instance of this class
 export class RmqModule{
-
+    static register({name}: RmqModuleOptions): DynamicModule{
+        // return a dynamic module
+        return {
+            module: RmqModule,
+            // register the rabbit mq service  via ClientsModule from nestjs/microservice with reff to  name received in register static method arguments.
+            imports: [
+                // registerAsyn takes array of services(object) we intend to define
+                ClientsModule.registerAsync([
+                    {
+                        name,
+                        // using useFactory to inject the ConfigService(needed for instantiating rabbit mq queue)
+                        useFactory: (configService: ConfigService) => ({
+                            transport: Transport.RMQ,
+                            // passing the required options need to instantiate the rabbit mq queue
+                            options: {
+                                // array of urls on which rabbitmq will be listening on
+                                urls: [configService.get<string>('RABBIT_MQ_URI')],
+                                // actual name of the queue that we are trying to register
+                                queue: configService.get<string>(`RABBIT_MQ_${name}_QUEUE`),
+                            },
+                        }),
+                        inject: [ConfigService],
+                    },
+                ]),
+            ],
+            // re-exporting the ClientsModule so that the consuming module i.e which imports it have access to the ClientsModule along with the registered rabbit mq  service that is created with this dynamic module
+            exports: [ClientsModule]
+        }
+    }
 }
