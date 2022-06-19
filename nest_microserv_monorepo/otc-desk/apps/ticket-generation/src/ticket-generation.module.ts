a78e46d3e3fc61f7860836c3e2af588577dea3d3
Jasmeet-1998 <jasmeetbali.dev.2021@gmail.com>
2022-06-19 18:48:49 +0530

    misc instantiate configService in tg microserv
    complete message:
    - as rabbit mq is dependent on config env's

--- nest_microserv_monorepo/otc-desk/apps/ticket-generation/src/ticket-generation.module.ts
+++ nest_microserv_monorepo/otc-desk/apps/ticket-generation/src/ticket-generation.module.ts
@@ -1,10 +1,22 @@
 import { RmqModule } from '@app/common';
 import { Module } from '@nestjs/common';
+import * as Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
 import { TicketGenerationController } from './ticket-generation.controller';
 import { TicketGenerationService } from './ticket-generation.service';
-
+ 
 @Module({
-  imports: [RmqModule],
+  // imporring env via ConfigModule as rabbit mq needs env's
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+      RABBIT_MQ_URI: Joi.string().required(),
+      RABBIT_MQ_TICKETGENERATION_QUEUE: Joi.string().required(),
+      }),
+    }),
+  RmqModule,
+],
   controllers: [TicketGenerationController],
   providers: [TicketGenerationService],
 })
