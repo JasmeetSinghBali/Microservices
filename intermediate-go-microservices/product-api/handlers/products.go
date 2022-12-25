cd8c7ff19b3c2e53553ef2583f92bb725bed1a2f
Jasmeet-1998 <devs.us.1984@gmail.com>
2022-12-25 13:44:50 +0530

    comm method checker, reroutes to specific method

--- intermediate-go-microservices/product-api/handlers/products.go
+++ intermediate-go-microservices/product-api/handlers/products.go
@@ -15,8 +15,24 @@ func NewProducts(tracer *log.Logger) *Products {
 	return &Products{tracer}
 }
 
+/*
+controller/handler , that calls restful routes a/c to the method types
+*/
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return
+	}
 
+	//catch all other request method
+	// try $ curl localhost:5000/products -XDELETE -v
+	rw.WriteHeader(http.StatusNotImplemented)
+}
+
+/*
+restful get products  method on Products handler struct
+*/
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	/*access the method in data package to get product list*/
 	listOfProducts := data.GetProducts()
 
