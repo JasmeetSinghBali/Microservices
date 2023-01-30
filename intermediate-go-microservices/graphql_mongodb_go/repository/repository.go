18691eee63fd62ad88d67fed9d360809e97ebeaa
Jasmeet-1998 <devs.us.1984@gmail.com>
2023-01-30 22:39:12 +0530

    gql-server: up

--- intermediate-go-microservices/graphql_mongodb_go/repository/repository.go
+++ intermediate-go-microservices/graphql_mongodb_go/repository/repository.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/Jasmeet-1998/Microservices/intermediate-go-microservices/graphql_mongodb_go/graph/model"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -17,7 +18,7 @@ type Repository struct {
 }
 
 func Connect() *Repository {
-	errEnv := godotenv.Load("../.env")
+	errEnv := godotenv.Load(".env")
 	if errEnv != nil {
 		log.Fatal("Error loading .env file")
 	}
@@ -43,4 +44,32 @@ func Connect() *Repository {
 		client: client,
 	}
 
+}
+
+// method- GetHub to the Repository struct , to get a donut hub by id
+func (db *Repository) GetHub(id string) *model.DonutHub {
+	var donuthub model.DonutHub
+	return &donuthub
+}
+
+// method - GetHubs to the Repository struct, to get all donut hubs
+func (db *Repository) GetHubs() []*model.DonutHub {
+	var donuthubs []*model.DonutHub
+	return donuthubs
+}
+
+// method - CreateDonutHub to the Repository struct, to create donut hub
+func (db *Repository) CreateDonutHub(hubInfo model.CreateDonutHubInput) *model.DonutHub {
+	var newDonutHub model.DonutHub
+	return &newDonutHub
+}
+
+// method - UpdateDonut to the Repository struct, to update donut hub
+func (db *Repository) UpdateDonutHub(hubId string, hubInfo model.UpdateDonutHubInput) *model.DonutHub {
+	var donutHub model.DonutHub
+	return &donutHub
+}
+
+func (db *Repository) DeleteDonutHub(hubId string) *model.DeleteDonutHubResponse {
+	return &model.DeleteDonutHubResponse{DeleteHubID: hubId}
 }
